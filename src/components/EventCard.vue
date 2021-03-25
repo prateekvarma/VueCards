e1ce9c963aae96016fb0374ab0ab809f5561f34c
Prateek Varma <prtk.varma@gmail.com>
2021-03-25 15:01:13 -0600

    created EventCard component, and nested it with EventList

---
+++ src/components/EventCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <hr>
+        <span>@{{ event.time }} {{ event.date }}</span>
+        <router-link :to="{name: 'event-show', params: {id: '1'}}">
+            <h4>{{ event.title }}</h4>
+        </router-link>
+        <span>{{ event.attendees.length }} Attendees</span>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            event: {
+                id: 1,
+                title: 'Beach Cleanup',
+                date: 'Tues Aug 19, 2021',
+                time: '6:00',
+                attendees: [
+                    { id: 'abc123', name: 'Ronnie James Dio' },
+                    { id: 'zyx345', name: 'Ozzy Ozbourne' }
+                ]
+            }
+        }
+    },
+}
+</script>
